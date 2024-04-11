8d5fd720474f6608b897432d45cbce1c3703a841
butchbutchart <42536650+butchbutchart@users.noreply.github.com>
2024-04-11 10:45:58 +0100

    Add files via upload

---
+++ Jump_Client_Report_v1.ps1
@@ -0,0 +1,28 @@
+﻿# Define the command to invoke btapi with parameters
+$command = ".\btapi --env-file vars.txt list jump-client"
+
+# Execute the command and capture the output
+$output = Invoke-Expression -Command $command
+
+# Display the output as an array
+Write-Host "Output as array:"
+$output
+
+# Get the current date and time
+$currentDateTime = Get-Date -Format "yyyyMMdd-HHmmss"
+
+# Define the filename with the current date and time
+$csvFileName = "jump_client_report_$currentDateTime.csv"
+
+# Convert the JSON output to PowerShell objects
+$jsonObjects = $output | ConvertFrom-Json
+
+# Select the properties you want to include in the CSV
+$selectedProperties = $jsonObjects | ForEach-Object {
+    $_ | Select-Object id, jump_group_id, name, fqdn, operating_system, public_ip, private_ip, console_user, last_connect_timestamp
+}
+
+# Export the selected properties to a CSV file
+$selectedProperties | Export-Csv -Path $csvFilePath -NoTypeInformation
+
+Write-Host "Output saved as $csvFileName"
