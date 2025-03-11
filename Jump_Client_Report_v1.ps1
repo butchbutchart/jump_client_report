dee32301c2d25aae0e2e16fb5ea781efe14cb232
butchbutchart <42536650+butchbutchart@users.noreply.github.com>
2025-03-11 11:59:50 +0000

    Update Jump_Client_Report_v1.ps1
    Removed reliance on API client

--- Jump_Client_Report_v1.ps1
+++ Jump_Client_Report_v1.ps1
@@ -1,28 +1,49 @@
-﻿# Define the command to invoke btapi with parameters
-$command = ".\btapi --env-file vars.txt list jump-client"
+$tokenUrl = "https://URL.beyondtrustcloud.com/oauth2/token";
+$baseUrl = "https://URL.beyondtrustcloud.com/api/config/v1"
+$client_id = "--";
+$secret = "--";
+$exportpath = "jumpclients.csv"
 
-# Execute the command and capture the output
-$output = Invoke-Expression -Command $command
+#endregion creds
+###########################################################################
 
-# Display the output as an array
-Write-Host "Output as array:"
-$output
+#region Authent 
+###########################################################################
 
-# Get the current date and time
-$currentDateTime = Get-Date -Format "yyyyMMdd-HHmmss"
+# Step 1. Create a client_id:secret pair
+$credPair = "$($client_id):$($secret)"
+# Step 2. Encode the pair to Base64 string
+$encodedCredentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($credPair))
+# Step 3. Form the header and add the Authorization attribute to it
+$headersCred = @{ Authorization = "Basic $encodedCredentials" }
+# Step 4. Make the request and get the token
+$responsetoken = Invoke-RestMethod -Uri "$tokenUrl" -Method Post -Body "grant_type=client_credentials" -Headers $headersCred
+$token = $responsetoken.access_token
+$headersToken = @{ Authorization = "Bearer $token" }
+# Step 5. Prepare the header for future request
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Content-Type", "application/json")
+$headers.Add("Accept", "application/json")
+$headers.Add("Authorization", "Bearer $token")
+#endregion
+###########################################################################
 
-# Define the filename with the current date and time
-$csvFileName = "jump_client_report_$currentDateTime.csv"
 
-# Convert the JSON output to PowerShell objects
-$jsonObjects = $output | ConvertFrom-Json
 
-# Select the properties you want to include in the CSV
-$selectedProperties = $jsonObjects | ForEach-Object {
-    $_ | Select-Object id, jump_group_id, name, fqdn, operating_system, public_ip, private_ip, console_user, last_connect_timestamp
-}
+# Invoke the REST method to get the list of group policies
+$jumpclients = Invoke-RestMethod "$baseUrl/jump-client" -Method 'GET' -Headers $headers
 
-# Export the selected properties to a CSV file
-$selectedProperties | Export-Csv -Path $csvFileName -NoTypeInformation
 
-Write-Host "Output saved as $csvFileName"
+
+
+$jumpclients | Select-Object id, jump_group_id, name, tag, public_site_address, comments, jump_policy_id, 
+needs_update, install_mode, is_quiet, connection_type, last_connect_timestamp, last_disconnect_timestamp, 
+fqdn, unavailable_reason, operating_system, public_ip, private_ip, console_user, expiration_timestamp, 
+is_lost, jump_group_type, customer_client_start_mode, hostname, max_offline_minutes, last_access_timestamp, 
+jumpoint_proxy, unattended_session_policy_id, attended_session_policy_id |
+Export-Csv -Path $exportpath -NoTypeInformation
+
+Write-Output "CSV exported to: $exportpath"
+
+
+
